92f05d06ed56b1076402f031f8947833a3516748
Sibusiso-M <sibu.mdlovu@gmail.com>
2024-11-30 22:00:42 +0200

    add rate limiting

--- redux-recipe-search-back-end/api/index.js
+++ redux-recipe-search-back-end/api/index.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  message:
+    "Too many API requests from this IP, please try again in 15 minutes.",
+});
 
 const envFile =
   process.env.NODE_ENV === "production"
@@ -11,6 +19,7 @@ const envFile =
 dotenv.config({ path: envFile });
 
 const app = express();
+app.use(limiter);
 const isProduction = process.env.NODE_ENV === "production";
 // Configure CORS
 const corsOptions = {
@@ -26,7 +35,7 @@ const corsOptions = {
       callback(null, true); // Allow all origins in development
     }
   },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  methods: "GET,HEAD,POST,OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
 };
 
@@ -37,52 +46,48 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Handle preflight requests
 
-app.get("/", async (req, res) => {
-  res.status(200).json('Express on vercel')
+app.get("/", async (_req, res) => {
+  res.status(200).json("Home");
 });
 
 app.post("/recipes", async (req, res) => {
-  const { searchKeyword, ingredients } = req.body;
-  const envFile =
-    process.env.NODE_ENV === "production"
-      ? ".env.production"
-      : ".env.development";
-  dotenv.config({ path: envFile });
-  const { EDAMAM_APP_ID: appId, EDAMAM_APP_KEY: appKey } = process.env;
+  try {
+    const { searchKeyword, ingredients } = req.body;
+    const envFile =
+      process.env.NODE_ENV === "production"
+        ? ".env.production"
+        : ".env.development";
+    dotenv.config({ path: envFile });
+    const { EDAMAM_APP_ID: appId, EDAMAM_APP_KEY: appKey } = process.env;
 
-  if (!appKey || !appId) {
-    res.status(500).json({ error: "API credentials are missing" });
-  } else if (
-    !searchKeyword ||
-    typeof searchKeyword !== "string" ||
-    searchKeyword.length < 3
-  ) {
-    res.status(400).json({
-      error: "Search keyword must be a valid string with 3+ characters",
-    });
-  } else if (!ingredients) {
-    res.status(400).json({
-      error:
-        "Ingredients should be defined (use a comma-separated list if multiple)",
-    });
-  } else {
-    const recipeApiUrl = "https://api.edamam.com/api/recipes/v2";
-    const urlParameters = `${recipeApiUrl}?q=${searchKeyword} ${ingredients.join(
-      " "
-    )}&app_id=${appId}&app_key=${appKey}&type=public`;
+    if (!appKey || !appId) {
+      res.status(500).json({ error: "API credentials are missing" });
+    } else if (!searchKeyword || typeof searchKeyword !== "string" || searchKeyword.length < 3) {
+      res.status(400).json({
+        error: "Search keyword must be a valid string with 3+ characters",
+      });
+    } else if (!ingredients) {
+      res.status(400).json({
+        error:
+          "Ingredients should be defined (use a comma-separated list if multiple)",
+      });
+    } else {
+      const recipeApiUrl = "https://api.edamam.com/api/recipes/v2";
+      const urlParameters = `${recipeApiUrl}?q=${searchKeyword} ${ingredients.join(
+        " "
+      )}&app_id=${appId}&app_key=${appKey}&type=public`;
 
-    try {
       const response = await axios.get(urlParameters);
       const recipes = response.data.hits.map(({ recipe }) => {
         const { image, ingredientLines, label } = recipe;
         return { image, ingredientLines, label };
       });
       res.status(200).json(recipes);
-    } catch (error) {
-      res.status(500).json({
-        error: error.response?.data || "Failed to fetch recipes",
-      });
     }
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch recipes",
+    });
   }
 });
 
@@ -96,9 +101,9 @@ if (!isProduction) {
 } else {
   const PORT = process.env.PORT || 8080;
 
-  const server = app.listen(PORT, '0.0.0.0', () => {
+  const server = app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
   });
-  
+
   server.timeout = 10000; // Increase to 10 seconds
 }
